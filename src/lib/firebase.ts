a8595106b2f30b8eb58dda5161cdfa33fd97d4ef
Zed Zure <zedzure@gmail.com>
2025-09-27 11:20:31 +0000

    add // Import the functions you need from the SDKs you need import { ini

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,14 +6,14 @@ import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration is read from environment variables
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyDOyZzlREsyirlpt1QkTZvDmzfILPV2Vt4",
+  authDomain: "dispensary-13773344-54196.firebaseapp.com",
+  projectId: "dispensary-13773344-54196",
+  storageBucket: "dispensary-13773344-54196.appspot.com",
+  messagingSenderId: "210293046378",
+  appId: "1:210293046378:web:2351647d3b963f9215de13"
 };
 
 // Initialize Firebase
