1676884a2bdeaf4b234f0b3d18c1f6517130efc5
Zed Zure <zedzure@gmail.com>
2025-09-27 11:45:36 +0000

    now its saying that my api key is not valid

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -8,12 +9,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDOyZzlREsyirlpt1QkTZvDmzfILPV2Vt4",
-  authDomain: "dispensary-13773344-54196.firebaseapp.com",
-  projectId: "dispensary-13773344-54196",
-  storageBucket: "dispensary-13773344-54196.appspot.com",
-  messagingSenderId: "210293046378",
-  appId: "1:210293046378:web:2351647d3b963f9215de13"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
