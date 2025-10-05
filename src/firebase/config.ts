2f356e24e6a42cd4281e5fac86356ce7b21e5518
Zed Zure <zedzure@gmail.com>
2025-10-05 19:38:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "dispensary-13773344-54196",
+  "appId": "1:210293046378:web:2351647d3b963f9215de13",
+  "apiKey": "AIzaSyDOyZzlREsyirlpt1QkTZvDmzfILPV2Vt4",
+  "authDomain": "dispensary-13773344-54196.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "210293046378"
+};
