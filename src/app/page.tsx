e2ed2283cb22765f8a7f2ada624060e7f2b0a2a7
Zed Zure <zedzure@gmail.com>
2025-10-05 19:22:46 +0000

    add // Import the functions you need from the SDKs you need import { ini

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import React from 'react';
 import { Suspense } from 'react';
 import { Header } from '@/components/header';
 import { Footer } from '@/components/footer';
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,27 +1,17 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth, initializeAuth, browserLocalPersistence } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDOyZzlREsyirlpt1QkTZvDmzfILPV2Vt4",
   authDomain: "dispensary-13773344-54196.firebaseapp.com",
   projectId: "dispensary-13773344-54196",
-  storageBucket: "dispensary-13773344-54196.appspot.com",
+  storageBucket: "dispensary-13773344-54196.firebasestorage.app",
   messagingSenderId: "210293046378",
   appId: "1:210293046378:web:2351647d3b963f9215de13"
 };
 
 // Initialize Firebase
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-
-// Initialize Firebase services
-export const auth = initializeAuth(app, {
-  persistence: browserLocalPersistence
-});
-
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const app = initializeApp(firebaseConfig);
