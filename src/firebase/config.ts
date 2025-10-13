5b089cb992d03d4bc687bdb9c7d605c7c4b50a35
Zed Zure <zedzure@gmail.com>
2025-10-13 17:18:43 +0000

    use this api key AIzaSyDKpU66lKVneXOK-KO16g_F675WUiG0lr8

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "dispensary-13773344-54196",
   "appId": "1:210293046378:web:2351647d3b963f9215de13",
-  "apiKey": "AIzaSyDOyZzlREsyirlpt1QkTZvDmzfILPV2Vt4",
+  "apiKey": "AIzaSyDKpU66lKVneXOK-KO16g_F675WUiG0lr8",
   "authDomain": "dispensary-13773344-54196.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "210293046378"
