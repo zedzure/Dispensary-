4ba2716a440f772867245d265ff53164a0e4d47d
Zed Zure <zedzure@gmail.com>
2025-10-13 19:34:42 +0000

    add api key // Import the functions you need from the SDKs you need impo

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,4 +5,5 @@ export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
